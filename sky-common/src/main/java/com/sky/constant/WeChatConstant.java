6e1551066e55839a4a65b306460fa6d3bed89236
fengyuanxiang <3224321794@qq.com>
2024-01-01 15:32:11 +0800

    微信登录

---
+++ sky-common/src/main/java/com/sky/constant/WeChatConstant.java
@@ -0,0 +1,8 @@
+package com.sky.constant;
+
+/**
+ * 微信的接口
+ */
+public class WeChatConstant {
+    public static final String WX_LOGIN = "https://api.weixin.qq.com/sns/jscode2session";
+}
---
+++ sky-server/src/main/java/com/sky/controller/user/UserController.java
@@ -0,0 +1,63 @@
+package com.sky.controller.user;
+
+import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.UserLoginDTO;
+import com.sky.entity.User;
+import com.sky.properties.JwtProperties;
+import com.sky.result.Result;
+import com.sky.service.UserService;
+import com.sky.utils.JwtUtil;
+import com.sky.vo.UserLoginVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@Slf4j
+@RequestMapping("/user/user")  //第一个user代表用户端，第二个user代表用户模块
+@Api(tags = "C端用户接口")
+
+
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    /**
+     * 微信登录
+     * @param userLoginDTO
+     * @return
+     */
+    @PostMapping("login")
+    @ApiOperation("微信登录")
+    public Result<UserLoginVO> login(@RequestBody UserLoginDTO userLoginDTO){
+        log.info("微信用户登录：{}",userLoginDTO.getCode());
+
+        //调用微信登录
+        User user = userService.wechatLogin(userLoginDTO);
+
+        //生成令牌
+        Map<String,Object> claims = new HashMap<>();
+        claims.put(JwtClaimsConstant.USER_ID,user.getId());
+        String token = JwtUtil.createJWT(jwtProperties.getUserSecretKey(), jwtProperties.getUserTtl(), claims);
+
+        UserLoginVO userLoginVO = UserLoginVO.builder()
+                .id(user.getId())
+                .openid(user.getOpenid())
+                .token(token)
+                .build();
+
+        return Result.success(userLoginVO);
+    }
+}
